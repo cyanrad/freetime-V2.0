4c71b21021802b676f5a8f87ba0ab0a115914226
Radwan Jabraouti <60131230+cyanrad@users.noreply.github.com>
2023-09-13 18:59:47 +0400

    init

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	lo "github.com/samber/lo"
+)
+
+type Period struct {
+	day   int
+	start int
+	end   int
+}
+
+func main() {
+	inputFiles, _ := initArgs()
+
+	scheduleData := [][]Period{}
+
+	for _, f := range inputFiles {
+		scheduleData = append(scheduleData, parsePeriodCsvFile(f))
+	}
+
+	flattenedSchedule := lo.Flatten(scheduleData)
+	byDayPeriodGroups := lo.GroupBy(flattenedSchedule, func(p Period) int { return p.day })
+
+	// no need to sort yet
+	// for _, group := range byDayPeriodGroups {
+	// 	sort.Slice(group, func(i, j int) bool {
+	// 		return group[i].start < group[j].start
+	// 	})
+	// }
+
+}
+
+func initArgs() ([]string, string) {
+	inputFilesString := ""
+	flag.StringVar(&inputFilesString, "f", "",
+		"input schedule csv files (expected format: \"file1, file2\")")
+	outputFile := ""
+	flag.StringVar(&outputFile, "o", "", "output file")
+	flag.Parse()
+
+	inputFiles := strings.Split(inputFilesString, ", ")
+	return inputFiles, outputFile
+}
+
+func parsePeriodCsvFile(fileName string) []Period {
+	fReader, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(fReader)
+	parsedValues, err := r.ReadAll()
+	if err != nil {
+		log.Println(err)
+	}
+
+	parsedData := []Period{}
+
+	for _, val := range parsedValues {
+		day, err := strconv.Atoi(val[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		start, err := strconv.Atoi(val[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		end, err := strconv.Atoi(val[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		parsedData = append(parsedData, Period{
+			day:   day,
+			start: start,
+			end:   end,
+		})
+	}
+
+	return parsedData
+}
